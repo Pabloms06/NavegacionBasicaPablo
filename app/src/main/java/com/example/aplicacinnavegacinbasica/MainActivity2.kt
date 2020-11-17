71cd55b6ab5530b04a43b1d4d677f628c3b623f9
Pablo Marcos Sánchez <pablomarcossanche06.pm@gmail.com>
2020-11-17 21:12:40 +0100

    Actuaización 2

--- app/src/main/java/com/example/aplicacinnavegacinbasica/MainActivity2.kt
+++ app/src/main/java/com/example/aplicacinnavegacinbasica/MainActivity2.kt
@@ -1,25 +1,20 @@
 package com.example.aplicacinnavegacinbasica
 
+
 import android.os.Bundle
 import android.widget.ImageView
-
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity2 : AppCompatActivity() {
-                companion object {
-                    const val boton2 = "b"
-                }
-                override fun onCreate(savedInstanceState: Bundle?) {
-                    super.onCreate(savedInstanceState)
-                    setContentView(R.layout.activity_main_2)
 
-                    val b = findViewById<ImageView>(R.id.imageView)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_2)
 
-                    val src = intent.getStringExtra(boton2)
+        val Imagen = findViewById<ImageView>(R.id.imageView)
 
-                    src?.let {
-                        b.setImageResource(R.drawable.logo)
-                    }
-                }
-            }
+           Imagen.setImageResource(R.drawable.logo)
+        }
+
+    }
--- app/src/main/java/com/example/aplicacinnavegacinbasica/MainActivity3.kt
+++ app/src/main/java/com/example/aplicacinnavegacinbasica/MainActivity3.kt
@@ -1,17 +1,31 @@
 package com.example.aplicacinnavegacinbasica
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity3 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_3)
 
 
+        val boton2 = findViewById<Button>(R.id.boton2)
         val texto = findViewById<EditText>(R.id.textoedit)
+        val texto2 = findViewById<TextView>(R.id.textView)
+
+        boton2.setOnClickListener {
+            val intent2 = Intent(this, MainActivity3::class.java)
+
+           val longitud = texto.length()
+            texto2.text = "El edit text de la activity anterior tenía una longitud de "+longitud+ " caracteres"
+
+            startActivity(intent2)
+        }
     }
 
-
-}
+    }
